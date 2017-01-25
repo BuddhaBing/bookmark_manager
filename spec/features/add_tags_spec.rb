99c00243a8190b3afa29ae45e89b81388cc49aed
Basile Koko <basile_koko@hotmail.com>
2017-01-25 12:25:04 +0000

    step 15 deploying to heroku

--- spec/features/add_tags_spec.rb
+++ spec/features/add_tags_spec.rb
@@ -1,16 +1,12 @@
-# As a time-pressed user
-# So that I can organise my many links into different categories for ease of search
-# I would like to tag links in my bookmark manager
-
-feature 'Adding tags' do
-  scenario 'I can add a single tag to a new link' do
-    visit('/new')
-    fill_in('url',   with: 'http://www.makersacademy.com/')
-    fill_in('title', with: 'Makers Academy')
-    fill_in('tags',  with: 'education')
-
+feature 'organise links into categories' do
+  scenario 'tag links' do
+    visit '/new'
+    fill_in 'title', with: 'BBC News'
+    fill_in 'url', with: 'www.bbc.co.uk'
+    fill_in 'tags', with: 'news'
     click_button 'Submit'
+    #expect(page).to have_content 'tags:news'
     link = Link.first
-    expect(link.tags.map(&:name)).to include('education')
+    expect(link.tags.map(&:name)).to include('news')
   end
 end
